c7a23d9191bb7c51231041997b482d8564938395
Alican Sekban <alicansekban@hotmail.com>
2025-01-25 15:10:00 +0300

    added home and detail screens, fetching popular movies for home screen

---
+++ MoviesIos/views/detail/MovieDetailScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailScreen.swift
+//  MoviesIos
+//
+//  Created by Alican SEKBAN on 25.01.2025.
+//
+
+import SwiftUI
+
+struct MovieDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailScreen()
+}
---
+++ MoviesIos/views/detail/MovieDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailViewModel.swift
+//  MoviesIos
+//
+//  Created by Alican SEKBAN on 25.01.2025.
+//
+
+import Foundation
