638d2285fbf5d42e31966397a7fb82d9ff6a6974
Alican Sekban <alicansekban@hotmail.com>
2025-07-19 23:41:28 +0300

    added AppRouter

---
+++ MoviesIos/navigation/AppRouter.swift
@@ -0,0 +1,20 @@
+//
+//  AppRouter.swift
+//  MoviesIos
+//
+//  Created by Alican SEKBAN on 19.07.2025.
+//
+
+import Foundation
+import SwiftUI
+
+class AppRouter : ObservableObject {
+    static let shared = AppRouter()
+    private init() {}
+    
+    
+    
+    func navigate(to view: some View) {
+        
+    }
+}
